114596d643187330c3fc68e0cbdbed22538c9631
beran24 <beran24cs@gmail.com>
2025-06-01 00:06:53 +0200

    Disable get mission withou picking missions

--- src/app/components/MissionPicker.tsx
+++ src/app/components/MissionPicker.tsx
@@ -38,6 +38,7 @@ export default function MissionPicker({ player }: { player: PlayerId }) {
       <button
         className="px-4 py-2 bg-custom-green rounded hover:bg-custom-light-green disabled:cursor-not-allowed disabled:bg-custom-disabled-bg disabled:text-custom-disabled-text"
         onClick={() => onHandleSave()}
+        disabled={!newMissions?.B || !newMissions?.A}
       >
         Get missions
       </button>
