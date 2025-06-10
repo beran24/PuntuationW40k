5d769c3dbab064a9a6a384748e3b63d8229e88a7
beran24 <beran24cs@gmail.com>
2025-06-11 01:18:02 +0200

    NF-localization add window

--- src/hooks/useCookies.tsx
+++ src/hooks/useCookies.tsx
@@ -2,7 +2,7 @@
 
 export default function useCookies() {
   const getCookieLocale = (): string => {
-    const match = document.cookie.match(/(^|;\s*)locale=([^;]*)/);
+    const match = window.document.cookie.match(/(^|;\s*)locale=([^;]*)/);
     return match ? decodeURIComponent(match[2]) : 'en';
   };
 
